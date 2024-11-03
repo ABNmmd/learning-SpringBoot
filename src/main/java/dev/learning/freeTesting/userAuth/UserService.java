27df6002fe7b3fd43bdbff577577905fe7a914d3
ABNmmd <moha.ww.ha@gmail.com>
2024-11-03 23:55:52 +0100

    User auth

--- src/main/java/dev/learning/freeTesting/userAuth/UserService.java
+++ src/main/java/dev/learning/freeTesting/userAuth/UserService.java
@@ -1,5 +1,7 @@
 package dev.learning.freeTesting.userAuth;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +10,15 @@ public class UserService {
     @Autowired
     private UserAuthRepository userRepository;
 
-    
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
 }
