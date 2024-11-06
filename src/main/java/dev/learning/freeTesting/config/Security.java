7bd36f7f2ca20c1ede0a55c3c670b7b7b8c8f674
ABNmmd <moha.ww.ha@gmail.com>
2024-11-07 00:12:21 +0100

    commit changes

--- src/main/java/dev/learning/freeTesting/config/Security.java
+++ src/main/java/dev/learning/freeTesting/config/Security.java
@@ -1,5 +1,23 @@
 package dev.learning.freeTesting.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import dev.learning.freeTesting.userAuth.UserService;
+
+@Configuration
+@EnableWebSecurity
 public class Security {
 
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+    }
 }
